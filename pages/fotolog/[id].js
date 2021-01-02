b1ff3c32938034083d5bc0ed54655ae375778fb6
Jeremiah Ashley <jeremiah.ashley@live.com>
2021-01-02 11:37:19 -0600

    Add head/meta to fotolog entry

--- pages/fotolog/[id].js
+++ pages/fotolog/[id].js
@@ -1,24 +1,40 @@
 import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
+import HeadMeta from '@components/HeadMeta'
+import { parseISO, format } from 'date-fns'
 
 const FotoLog = ({ fotoLog }) => {
-  return (
-    <article className="prose dark:prose-dark max-w-none">
-      {fotoLog.photos.map((photo) => (
-        <img src={photo.url} alt="" key={photo.id} />
-      ))}
+  let createdAtDate = parseISO(fotoLog.createdAt)
+  createdAtDate = format(createdAtDate, `MMMM dd, yyyy`)
 
-      {fotoLog.markdown.length >= 1 && (
-        <section>
-          {fotoLog.markdown.map((markdownBlock) => (
-            <ReactMarkdown
-              key={markdownBlock.substr(0, 10)}
-              children={markdownBlock}
-            />
-          ))}
-        </section>
-      )}
-    </article>
+  const pluralMetaString =
+    fotoLog.photos.length >= 2 ? `these photos` : `this photo`
+
+  return (
+    <>
+      <HeadMeta
+        title={`Photo on ${createdAtDate}`}
+        description={`Howdy! Check out ${pluralMetaString} I took on ${createdAtDate}`}
+        imageUrl={fotoLog.photos[0].url}
+      />
+
+      <article className="prose dark:prose-dark max-w-none">
+        {fotoLog.photos.map((photo) => (
+          <img src={photo.url} alt="" key={photo.id} />
+        ))}
+
+        {fotoLog.markdown.length >= 1 && (
+          <section>
+            {fotoLog.markdown.map((markdownBlock) => (
+              <ReactMarkdown
+                key={markdownBlock.substr(0, 10)}
+                children={markdownBlock}
+              />
+            ))}
+          </section>
+        )}
+      </article>
+    </>
   )
 }
 
@@ -49,6 +65,7 @@ export const getStaticProps = async ({ params }) => {
           fotoLog (where: {id: "${params.id}"}) {
             id
             markdown
+            createdAt
             photos {
               id
               url(transformation: {
