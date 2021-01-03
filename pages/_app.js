c6abac8ca774d1b4d6fbf2383fed049c32645348
Jeremiah Ashley <jeremiah.ashley@live.com>
2021-01-03 01:27:29 -0600

    Remove favicon links in head thatn broke everything

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,27 @@
-import '../styles/tailwind.css'
-import DefaultLayout from '../layouts/default'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { persistWithLocalStorage } from 'react-query/persist-localstorage-experimental'
+import Head from 'next/head'
+import '../styles/tailwind.css'
+import DefaultLayout from '../layouts/default'
 
 const queryClient = new QueryClient()
 persistWithLocalStorage(queryClient)
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <DefaultLayout>
-        <Component {...pageProps} />
-        {/*<ReactQueryDevtools initialIsOpen={false} />*/}
-      </DefaultLayout>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <DefaultLayout>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </DefaultLayout>
+      </QueryClientProvider>
+    </>
   )
 }
 
