470e54f6f457f33bb3a4eefb5ec655046db87fe9
Jeremiah Ashley <jeremiah.ashley@live.com>
2021-01-07 12:50:28 -0600

    Add Hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { persistWithLocalStorage } from 'react-query/persist-localstorage-experimental'
+import { hotjar } from 'react-hotjar'
 import Head from 'next/head'
 import '../styles/tailwind.css'
 import DefaultLayout from '../layouts/default'
+
+if (typeof window !== `undefined`) {
+  hotjar.initialize(2185761, 6)
+}
 
 const queryClient = new QueryClient()
 persistWithLocalStorage(queryClient)
